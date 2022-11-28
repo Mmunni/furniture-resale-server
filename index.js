e525b2aed63434cb6244b25010d05736ba7f3bad
Mmunni <masumamunni20@gmail.com>
2022-11-29 01:28:23 +0600

    jwt token updated

--- index.js
+++ index.js
@@ -16,10 +16,42 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ufiao9v.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next) {
+
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+      return res.status(401).send('unauthorized access');
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+      if (err) {
+          return res.status(403).send({ message: 'forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+  })
+
+}
+
 async function run() {
   try {
       const categoriesCollection = client.db('resaleFurniture').collection('categories');
       const productsCollection = client.db('resaleFurniture').collection('products');
+
+       // NOTE: make sure you use verifyAdmin after verifyJWT
+       const verifyAdmin = async (req, res, next) => {
+        const decodedEmail = req.decoded.email;
+        const query = { email: decodedEmail };
+        const user = await usersCollection.findOne(query);
+
+        if (user?.role !== 'admin') {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        next();
+    }
 
      app.get('/categories', async(req, res) => {
       const query = {};
